5b8ec770d816681588a84bbc0423e8396953d82b
dlxkhang <khangduong0902@gmail.com>
2021-11-24 22:25:24 +0700

    setup connect db

--- home_selling/app/models/index.js
+++ home_selling/app/models/index.js
@@ -1,17 +1,6 @@
 /** @format */
 
 const mongoose = require('mongoose');
-async function connect() {
-    try {
-        await mongoose.connect(process.env.DB_CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: process.env.DB_NAME,
-        });
-        console.log('Connected to DB successfully');
-    } catch (error) {
-        console.log('Connected to DB failed');
-    }
-}
 
-module.exports = {connect};
+
+module.exports = mongoose;
