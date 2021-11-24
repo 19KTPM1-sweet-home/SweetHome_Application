3c6debdb260ab63b16f3b01a51a2526b9f05ded0
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-11-24 22:36:41 +0700

    Update data.js

--- data_generation/data.js
+++ data_generation/data.js
@@ -1,36 +1,52 @@
 db = connect( 'mongodb://localhost:27017/sweethomedb' );
 db.category.insertMany([{
-  "_id": ObjectId("619d0b8dff7c23ca498ce404"),
+  "_id": {
+    "$oid": "619d0b8dff7c23ca498ce404"
+  },
   "name": "Home",
   "properties": [
-      ObjectId( "619d001cff7c23ca498ce3ce")
+    {
+      "$oid": "619d001cff7c23ca498ce3ce"
+    }
   ]
 }]);
 db.comments.insertMany([{
-  "_id": ObjectId("619d0422ff7c23ca498ce3e4"),
+  "_id": {
+    "$oid": "619e4933f28599d4ef895056"
+  },
   "authorName": "Nguyen Van A",
-  "postId": "619d001cff7c23ca498ce3ce",
-  "content": "What a beautiful house!",
+  "postId": {
+    "$oid": "619d001cff7c23ca498ce3ce"
+  },
   "postDate": {
-    "$date": "2015-07-03T00:00:00Z"
-  }
+    "$date": "2015-01-11T17:00:00Z"
+  },
+  "content": "my dream house"
 }]);
 db.customers.insertMany([{
-  "_id": ObjectId("619d012dff7c23ca498ce3df"),
+  "_id": {
+    "$oid": "619d012dff7c23ca498ce3df"
+  },
   "username": "nva123",
   "password": "123456",
   "fullName": "Nguyen Van A",
   "address": "227 Nguyen Van Cu",
   "favorite": [
-      ObjectId("619d001cff7c23ca498ce3ce")
+    {
+      "$oid": "619d001cff7c23ca498ce3ce"
+    }
   ],
   "email": "\"nva123@gmail.com",
   "schedule": [
-      ObjectId("619d088aff7c23ca498ce3f3")
+    {
+      "$oid": "619d088aff7c23ca498ce3f3"
+    }
   ]
 }]);
 db.properties.insertMany([{
-  "_id":ObjectId("619d001cff7c23ca498ce3ce"),
+  "_id": {
+    "$oid": "619d001cff7c23ca498ce3ce"
+  },
   "name": "Golden Dragon",
   "address": "227 Nguyen Van Cu",
   "description": "This is a house",
@@ -40,30 +56,42 @@ db.properties.insertMany([{
     "Built in 2011",
     "Forced air gar"
   ],
-  "price": "3500000000",
+  "price": 3500000000,
   "seller": {
     "name": "Le Van A",
     "phoneNumber": "0123456789",
     "email": "lva123@gmail.com"
   },
-  "rate": "4",
+  "rate": 4,
   "status": "For Sale",
   "category": {
     "name": "Home",
-    "categoryId": ObjectId("619d0e424317eda2eb700a18")
-  }
+    "categoryId": {
+      "$oid": "619d0e424317eda2eb700a18"
+    }
+  },
+  "detailImage": [
+    "../images/living-room.jpg",
+    "../images/bedroom.jpg",
+    "../images/outdoor.jpg"
+  ],
+  "previewImage": "../../images/house1.jpg"
 }]);
 db.schedules.insertMany([{
-  "_id": ObjectId("619d088aff7c23ca498ce3f3"),
+  "_id": {
+    "$oid": "619e4e5ff28599d4ef895065"
+  },
   "seller": {
     "name": "Le Van A",
     "phoneNumber": "0123456789",
     "email": "lva123@gmail.com"
   },
-  "propertyId": "619d001cff7c23ca498ce3ce",
-  "appoinmentDate": {
-    "$date": "2017-04-01T17:00:00Z"
+  "propertyId": {
+    "$oid": "619d001cff7c23ca498ce3ce"
   },
   "visitForm": "In Person",
-  "ack": "pending"
+  "ack": "pending",
+  "appointmentDate": {
+    "$date": "2017-04-01T17:00:00Z"
+  }
 }]);
