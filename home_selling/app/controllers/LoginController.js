1374c4c156bd960c629d4797c96041dd7417674a
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-11-25 16:40:22 +0700

    test

--- home_selling/app/controllers/LoginController.js
+++ home_selling/app/controllers/LoginController.js
@@ -1,7 +1,14 @@
-class LoginController{
+const User = require('../models/User');
+
+
+class LoginController {
     //[GET] /login
-    show(req, res, next){
-        res.render('login', {layout: false});
+    show(req, res, next) {
+        User.findOne({})
+            .then((user) => {
+                    res.render('login', { name: user.fullname});
+                })
+            .catch()
     }
 }
 module.exports = new LoginController();
