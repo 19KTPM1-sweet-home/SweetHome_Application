d90e726cf417f9cc98ec1213ff15c7d467773780
dlxkhang <khangduong0902@gmail.com>
2022-04-11 11:24:37 +0700

    change bcrypt library version

--- home_selling/app/services/userService.js
+++ home_selling/app/services/userService.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cloudinary = require("../../cloudinary/cloudinary");
 const streamifier = require("streamifier");
 const randomString = require("randomstring");
